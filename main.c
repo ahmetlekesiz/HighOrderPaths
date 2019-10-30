8bcf0ba82d9337b7de137ad4a1b9f2c75ace559e
ahmetlekesiz <ahmetlekesiz@gmail.com>
2019-10-31 01:27:56 +0300

    Sıralı ekleme düzenlendi.

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include<locale.h>
 
 struct Document {
     char DocumentName[50];
@@ -29,10 +30,13 @@ void getFilesRecursively(char *path, Term **root);
 void getCategory(char *path, char *category);
 void printFile(char *filePath, Term **root);
 void addWordIntoMasterLinkedList(Term **root, char* word);
+int checkIfWordAlreadyExist(Term *root, char *word);
 void printMasterLinkedList(Term *root);
 
 int main() {
+    setlocale(LC_ALL, "Turkish");
     Term *root = NULL;
+
 
     // Directory path of categories
     char path[500];
@@ -42,19 +46,11 @@ int main() {
     scanf("%s", path);
 
     getFilesRecursively(path, &root);
-    //char category[50];
-    //getCategory(path, category);
-    //printf("%s", category);
-
     printMasterLinkedList(root);
- /*   addWordIntoMasterLinkedList(&root, "beril");
-    addWordIntoMasterLinkedList(&root, "mehmet");
-    addWordIntoMasterLinkedList(&root, "ahmet");
-    addWordIntoMasterLinkedList(&root, "ceyda");
-    addWordIntoMasterLinkedList(&root, "zeynep");
 
-    printMasterLinkedList(root);*/
-
+/*
+   master linked liste e eklemede problem yok ancak, daha önce olup olmadığını kontrol edemiyorum.
+*/
     return 0;
 }
 
@@ -73,7 +69,7 @@ void getFilesRecursively(char *basePath, Term **root)
     {
         if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0)
         {
-            int length = strlen(dp->d_name);
+            int length;
             char extension[5];
 
             // Construct new path from our base path
@@ -93,9 +89,6 @@ void getFilesRecursively(char *basePath, Term **root)
             //Is that folder or txt file?
             if(strncmp(extension, ".txt", 4) == 0){
                 printFile(path, root);
-            } else {
-               // addCategory(path);
-               //Kategoriler depolayacağın bir linked list implement et.
             }
 
             getFilesRecursively(path, root);
@@ -122,6 +115,7 @@ void printFile(char *filePath, Term **root){
             getCategory(path, category);
             printf("Kelime: %s, Kategori: %s\n", word, category);
             addWordIntoMasterLinkedList(root, word);
+            memset(word, 0, sizeof(word));
         }
         fclose(in_file);
     }
@@ -139,7 +133,6 @@ void getCategory(char *path, char *category){
     while (ptr != NULL)
     {
         strncpy(words[wordCounter], ptr, strlen(ptr));
-        //printf("'%s'\n", ptr);
         ptr = strtok(NULL, delim);
         wordCounter++;
     }
@@ -147,45 +140,45 @@ void getCategory(char *path, char *category){
     strncpy(category, words[wordCounter-2], strlen(words[wordCounter-2]));
 }
 
-void addWordIntoMasterLinkedList(Term **root, char* word){
-    //Check if MLL is empty or not.
-    Term *iter = *root;
+void addWordIntoMasterLinkedList(Term **root, char *word){
+    //If MLL is empty.
     if(*root == NULL){
         (*root) = (Term*)malloc(sizeof(Term));
-        strncpy((*root)->Word, word, strlen(word));
+        strcpy((*root)->Word, word);
         (*root)->NextTerm = NULL;
         return;
-    }
-    //If is not empty, add the word to end of the list
-    else
-    {
-        //Create temp term to add.
-        Term *temp = (Term*)malloc(sizeof(Term));
-        temp->NextTerm = NULL;
-        strncpy(temp->Word, word, strlen(word));
-
-        //Add into list sorted.
-        //If the word less than root, change the root.
+    }else if(checkIfWordAlreadyExist(*root, word) == 0){
+        //ilk elemandan kücük durumu
         if(strcmp(word, (*root)->Word) < 0){
-            temp->NextTerm = *root;
+            //change the root
+            Term *temp = (Term*)malloc(sizeof(Term));
+            strcpy(temp->Word, word);
+            temp->NextTerm = (*root);
             *root = temp;
             return;
-        }//If the word between word, find its place.
-        else
-        {
-            while(iter->NextTerm != NULL && strcmp(word, iter->NextTerm->Word) < 0){
-                iter = iter->NextTerm;
-            }
-            //Add to end of the list.
-            if(iter->NextTerm == NULL){
-                //sona ekle
-                iter->NextTerm = temp;
-            }else{
-                temp->NextTerm = iter->NextTerm;
-                iter->NextTerm = temp;
-            }
         }
+        Term *iter = *root;
+        while(iter->NextTerm != NULL && strcmp(word, iter->NextTerm->Word) > 0){
+            iter = iter->NextTerm;
+        }
+        Term *temp = (Term*)malloc(sizeof(Term));
+        strcpy(temp->Word, word);
+        temp->NextTerm = iter->NextTerm;
+        iter->NextTerm = temp;
+        return;
     }
+
+}
+
+int checkIfWordAlreadyExist(Term *root, char *word){
+    Term *iter = *root;
+    while(iter->NextTerm != NULL){
+        if(strcmp(iter->Word, word) == 0){
+            return 1;
+        }
+        iter = iter->NextTerm;
+    }
+    return 0;
 }
 
 void printMasterLinkedList(Term *root){
@@ -195,3 +188,10 @@ void printMasterLinkedList(Term *root){
         iter = iter->NextTerm;
     }
 }
+
+void makeArrayEmpty(char *word){
+    size_t length = strlen(word);
+    for (int i = 0; i < length; ++i) {
+        word[i] = "";
+    }
+};
